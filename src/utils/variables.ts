cc209c196159385ca779f4e65a8fb4183a7cf687
Munyra Sírio <munyrasirio@gmail.com>
2023-05-07 18:06:35 -0300

    feat: weekdays constant

---
+++ src/utils/variables.ts
@@ -0,0 +1,9 @@
+export const weekDays = [
+  { value: "0", title: "Domingo", acronym: "D" },
+  { value: "1", title: "Segunda-feira", acronym: "S" },
+  { value: "2", title: "Terça-feira", acronym: "T" },
+  { value: "3", title: "Quarta-feira", acronym: "Q" },
+  { value: "4", title: "Quinta-feira", acronym: "Q" },
+  { value: "5", title: "Sexta-feira", acronym: "S" },
+  { value: "6", title: "Sábado", acronym: "S" },
+];
