e1af647719922701fffce29e5de4fe2f9c12ef48
Munyra Sírio <munyrasirio@gmail.com>
2023-05-07 18:08:08 -0300

    feat: selectitem component

---
+++ src/components/Form/SelectItem.tsx
@@ -0,0 +1,18 @@
+import * as RSelect from "@radix-ui/react-select";
+import { Check } from "phosphor-react";
+
+interface ISelectItem {
+  title: string;
+  id: string;
+}
+
+const SelectItem = ({ title, id }: ISelectItem) => (
+  <RSelect.Item value={id} className="relative py-1 pl-6 cursor-pointer">
+    <RSelect.ItemIndicator className="absolute inset-0 py-2 text-violet-500">
+      <Check />
+    </RSelect.ItemIndicator>
+    <RSelect.ItemText>{title}</RSelect.ItemText>
+  </RSelect.Item>
+);
+
+export default SelectItem;
