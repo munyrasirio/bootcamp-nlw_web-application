99c3f8224c8425dfa3795928dbd79d8a3a2c0545
Munyra Sírio <munyrasirio@gmail.com>
2023-05-07 18:07:43 -0300

    feat: create ad dialog component

--- src/components/CreateAdBanner/index.tsx
+++ src/components/CreateAdBanner/index.tsx
@@ -1,26 +1,23 @@
 import { MagnifyingGlassPlus } from "phosphor-react";
 import * as RDialog from "@radix-ui/react-dialog";
-import { Dialog } from "./Dialog";
 
-export const CreateAdBanner = () => (
-  <RDialog.Root>
-    <div className="bg-primaryGradient self-stretch rounded-lg overflow-hidden pt-2">
-      <div className="bg-[#2A2634] flex justify-between items-center px-8 py-6">
-        <div>
-          <h2 className="text-white font-black text-2xl block">
-            Não encontrou seu duo?
-          </h2>
-          <p className="text-zinc-400 block">
-            Publique um anúncio para encontrar novos players!
-          </p>
-        </div>
-        <RDialog.Trigger className="bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3 py-3 px-4">
-          <MagnifyingGlassPlus size={24} />
-          Publicar anúncio
-        </RDialog.Trigger>
+const CreateAdBanner = () => (
+  <div className="bg-primaryGradient self-stretch rounded-lg overflow-hidden pt-2">
+    <div className="bg-[#2A2634] flex justify-between items-center px-8 py-6">
+      <div>
+        <h2 className="text-white font-black text-2xl block">
+          Não encontrou seu duo?
+        </h2>
+        <p className="text-zinc-400 block">
+          Publique um anúncio para encontrar novos players!
+        </p>
       </div>
+      <RDialog.Trigger className="bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3 py-3 px-4">
+        <MagnifyingGlassPlus size={24} />
+        Publicar anúncio
+      </RDialog.Trigger>
     </div>
-
-    <Dialog />
-  </RDialog.Root>
+  </div>
 );
+
+export default CreateAdBanner;
